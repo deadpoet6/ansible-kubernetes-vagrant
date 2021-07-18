49de91e789a117fe87602805f27a78afaf84ae84
Manoj Reddy Yalamareddy <manoj.r.yalamareddy@accenture.com>
2021-07-18 02:27:58 +0200

    new changes

--- k3s/ansible/apply-resources.sh
+++ k3s/ansible/apply-resources.sh
@@ -1,2 +1,9 @@
 vagrant scp master-1:~/k3s.yaml ~/.kube/config
 kubectl apply -f ./apps/components.yaml
+kubectl create ns keycloak
+kubectl config set-context --current --namespace=keycloak
+kubectl apply -f ./apps/postgres/manifest.yaml
+sleep 60s
+kubectl apply -f ./apps/keycloak/manifest.yaml
+kubectl create ns argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
